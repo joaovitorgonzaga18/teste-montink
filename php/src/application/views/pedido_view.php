7ca696acbc1e86238b41c7aa3ac8d9b8da8f12b5
João Vitor Gonzaga Jota <54511843+joaovitorgonzaga18@users.noreply.github.com>
2025-05-16 14:28:20 -0300

    New data for products modal

--- php/src/application/views/pedido_view.php
+++ php/src/application/views/pedido_view.php
@@ -1,15 +1,22 @@
 <div class="row">
     <div class="col-md-12">
         <div class="modal-header">
-            <h5 class="modal-title h4" id="myLargeModalLabel">Pedido #<?= $pedido['id'] ?> - <?= $pedido['data_pedido'] ?> - CEP <?= $pedido['cep'] ?></h5>
+            <h5 class="modal-title h4" id="myLargeModalLabel">Pedido #<?= $pedido['id'] ?></h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">×</span>
             </button>
         </div>
 
         <div class="modal-body">
-            <div class>
-
+            <div class="row">
+                <div class="col-md-12">
+                    <h5 class="card-title">Informações Adicionais</h5>
+                    <ul>
+                        <li>Data de criação: <?= $pedido['data_pedido'] ?></li>
+                        <li>CEP: <?= $pedido['cep'] ?></li>
+                        <li>Cupom aplicado: <?= $pedido['cupom'] ?></li>
+                    </ul>
+                </div>
             </div>
             <div class="row">
                 <div class="col-md-12">
@@ -31,10 +38,12 @@
                                 <th>Frete</td>
                                 <td><?= $pedido['frete'] ?></td>
                             </tr>
-                            <tr class="text-right">
-                                <th>Desconto</th>
-                                <td><?= $pedido['desconto'] ?></td>
-                            </tr>
+                            <?php if ($pedido['cupom'] != '---') { ?>
+                                <tr class="text-right">
+                                    <th>Desconto (<?= $pedido['cupom'] ?>)</th>
+                                    <td><?= $pedido['desconto'] ?></td>
+                                </tr>
+                            <?php } ?>
                             <tr class="text-right" style="border-top: solid 2px black">
                                 <th>Valor total</th>
                                 <td><?= $pedido['preco'] ?></td>
